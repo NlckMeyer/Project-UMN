a4361b55c5d9b73c9bd3d6ea34eac92d7552d8d7
Nick <125489573+NlckMeyer@users.noreply.github.com>
2024-02-08 13:44:13 -0600

    Update Java.java

--- Java.java
+++ Java.java
@@ -1,8 +1,5 @@
-public class Main{
-  public stastic void main(string[] args) 
-  {
-    
-    System.out.println("In the works");
+public class Main {
+  public static void main(String[] args) {
+      System.out.println("In the works");
   }
-  
 }
